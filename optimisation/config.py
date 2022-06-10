d209dbf9fffef1113d5ee7ffd43392a0bc7c52a4
Cheng Cui <cheng.cui.95@gmail.com>
2022-06-10 15:00:18 +0100

    Add config.py

---
+++ optimisation/config.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+from ._simulator import _energyplus_default_root
+
+from ._tools import AnyPath
+
+CONFIG = {"exec.energyplus": None, "exec.readvars": None, "schema.energyplus": None}
+
+
+def config_energyplus(
+    version_parts: tuple[int, int, int] = None,
+    root: AnyPath = None,
+    energyplus_exec: AnyPath = None,
+    readvars_exec: AnyPath = None,
+    schema: AnyPath = None,
+) -> None:
+    if version_parts is not None:
+        root = _energyplus_default_root(*version_parts)
+
+    if root is not None:
+        root = Path(root)
+        energyplus_exec = root / "energyplus"
+        readvars_exec = root / "PostProcess" / "ReadVarsESO"
+        schema = root / "Energy+.idd"
+
+    CONFIG["exec.energyplus"] = Path(energyplus_exec).resolve(strict=True)
+    CONFIG["exec.readvars"] = Path(readvars_exec).resolve(strict=True)
+    CONFIG["schema.energyplus"] = Path(schema).resolve(strict=True)
