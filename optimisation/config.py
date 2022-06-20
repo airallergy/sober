d997be66efc6754cea9fcc8a70fa6cf970f73ba8
Cheng Cui <cheng.cui.95@gmail.com>
2022-06-20 18:13:10 +0100

    Add config_energyplus fallback

--- optimisation/config.py
+++ optimisation/config.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 from ._simulator import _default_root
 
+
+from typing import Any
 from ._tools import AnyStrPath
 
-_CONFIG = {
+_CONFIG: dict[str, Any] = {
     "exec.energyplus": None,
     "exec.epmacro": None,
     "exec.readvars": None,
@@ -15,6 +17,7 @@ def config_energyplus(
     version_parts: tuple[int, int, int] = None,
     root: AnyStrPath = None,
     energyplus_exec: AnyStrPath = None,
+    epmacro_exec: AnyStrPath = None,
     readvars_exec: AnyStrPath = None,
     schema: AnyStrPath = None,
 ) -> None:
@@ -28,7 +31,17 @@ def config_energyplus(
         readvars_exec = root / "PostProcess" / "ReadVarsESO"
         schema = root / "Energy+.idd"
 
-    _CONFIG["exec.energyplus"] = Path(energyplus_exec).resolve(strict=True)
-    _CONFIG["exec.epmacro"] = Path(epmacro_exec).resolve(strict=True)
-    _CONFIG["exec.readvars"] = Path(readvars_exec).resolve(strict=True)
-    _CONFIG["schema.energyplus"] = Path(schema).resolve(strict=True)
+    if (
+        (energyplus_exec is not None)
+        and (epmacro_exec is not None)
+        and (readvars_exec is not None)
+        and (schema is not None)
+    ):
+        _CONFIG["exec.energyplus"] = Path(energyplus_exec).resolve(strict=True)
+        _CONFIG["exec.epmacro"] = Path(epmacro_exec).resolve(strict=True)
+        _CONFIG["exec.readvars"] = Path(readvars_exec).resolve(strict=True)
+        _CONFIG["schema.energyplus"] = Path(schema).resolve(strict=True)
+    else:
+        raise ValueError(
+            "One of version_parts, root, (energyplus_exec, epmacro_exec, readvars_exec, schema) needs to be provided."
+        )
