dfd5223eae41fcd7e5032d8cfe6c730bde7f43d6
Cheng Cui <cheng.cui.95@gmail.com>
2022-06-21 23:15:06 +0100

    Fix _to_pymoo in problem.py

--- optimisation/problem.py
+++ optimisation/problem.py
@@ -1,37 +1,37 @@
 from io import StringIO
 from pathlib import Path
 from collections.abc import Sequence
-from typing import TypeVar, Callable, Iterable, Optional
+from typing import Callable, Iterable
 
+import numpy as np
 from eppy import openidf
+from numpy.typing import NDArray
 from pymoo.core.problem import Problem as _PymooProblem
 
 from .config import _CONFIG
 from ._tools import AnyStrPath
+from ._evaluator import _evaluate
+from .collector import _Collector
 from .parameters import _Parameter
 from ._simulator import _split_model
-
-AnyReals = TypeVar("AnyReals", bound=float | Sequence[float])
 
 
 class PymooProblem(_PymooProblem):
     def __init__(
         self,
-        evaluator: Callable,
         n_var: int,
         n_obj: int,
         n_constr: int,
-        xl: AnyReals,
-        xu: AnyReals,
-        callback: Callable,
+        xl: NDArray[np.float_],
+        xu: NDArray[np.float_],
+        callback: Callable | None,
     ) -> None:
-        self.evaluator = evaluator
         super().__init__(
             n_var=n_var, n_obj=n_obj, n_constr=n_constr, xl=xl, xu=xu, callback=callback
         )
 
     def _evaluate(self, x, out, *args, **kwargs) -> None:
-        ...
+        _evaluate
 
 
 #############################################################################
@@ -41,26 +41,27 @@ class Problem:
     model_file: Path
     weather: _Parameter
     parameters: tuple[_Parameter, ...]
-    # collector: Sequence[_Collector]
-    callback: Optional[Callable] = None
+    objectives: tuple[_Collector, ...]
+    constraints: tuple[_Collector, ...]
+    callback: Callable | None = None
     _model_type: str
     _tagged_model: str
-    # objectives: Sequence[_Objective] = field(init=False)
-    # constraints: Sequence[_Constraint] = field(init=False)
 
     def __init__(
         self,
         model_file: AnyStrPath,
         weather: _Parameter,
         parameters: Iterable[_Parameter],
-        # collector: Iterable[_Collector],
-        callback: Optional[Callable] = None,
-        # objectives: Sequence[_Objective] = field(init=False),
-        # constraints: Sequence[_Constraint] = field(init=False),
+        objectives: Iterable[_Collector],
+        constraints: Iterable[_Collector] = (),
+        callback: Callable | None = None,
     ) -> None:
         self.model_file = Path(model_file)
         self.weather = weather
         self.parameters = tuple(parameters)
+        self.objectives = tuple(objectives)
+        self.constraints = tuple(constraints)
+        self.callback = callback
 
         self._model_type = self.model_file.suffix
         if self._model_type not in ("idf", "imf"):
@@ -80,13 +81,16 @@ class Problem:
 
         self._tagged_model = macros + idf.idfstr()
 
-    # def _to_pymoo(self) -> PymooProblem:
-    #     return PymooProblem(
-    #         self.evaluator,
-    #         len(self.parameters),
-    #         len(self.objectives),
-    #         len(self.constraints),
-    #         tuple(parameter.low for parameter in self.parameters),
-    #         tuple(parameter.high for parameter in self.parameters),
-    #         self.callback,
-    #     )
+    def _to_pymoo(self) -> PymooProblem:
+        return PymooProblem(
+            len(self.parameters),
+            len(self.objectives),
+            len(self.constraints),
+            np.fromiter(
+                (parameter.low for parameter in self.parameters), dtype=np.float_
+            ),
+            np.fromiter(
+                (parameter.high for parameter in self.parameters), dtype=np.float_
+            ),
+            self.callback,
+        )
