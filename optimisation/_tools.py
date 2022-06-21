421cd3c3eb716776db8efe5773b2218268e28206
Cheng Cui <cheng.cui.95@gmail.com>
2022-06-21 15:29:13 +0100

    Remove dataclass in _tools.py

--- optimisation/_tools.py
+++ optimisation/_tools.py
@@ -5,15 +5,3 @@ from typing import TypeAlias
 
 AnyStrPath: TypeAlias = str | PathLike[str]
 # AnyRArray: TypeAlias = _ArrayLikeFloat_co
-
-DATACLASS_PARAMS = {
-    "init": True,
-    "repr": False,
-    "eq": False,  # not work for float("nan")
-    "order": False,  # see 'eq'
-    "unsafe_hash": False,
-    "frozen": False,
-    "match_args": False,
-    "kw_only": False,
-    # "slots": True,  # explicit super(), see https://bugs.python.org/issue46404, https://stackoverflow.com/a/1817840
-}
