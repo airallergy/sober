53b2edc48aec42debd8176f60ad183b362d156d7
Cheng Cui <cheng.cui.95@gmail.com>
2022-07-04 22:12:44 +0100

    Update _multiplier.py using parameters manager

--- optimisation/_multiplier.py
+++ optimisation/_multiplier.py
@@ -2,30 +2,32 @@ from pathlib import Path
 from itertools import product
 
 from .results import _ResultsManager
+from .parameters import AnyIntParameter, _ParametersManager
 from ._evaluator import _product_evaluate, _parallel_evaluate
-from .parameters import WeatherParameter, AnyIntModelParameter
 
 
 def _multiply(
     tagged_model: str,
-    weather: WeatherParameter,
-    parameters: tuple[AnyIntModelParameter, ...],
+    parameters_manager: _ParametersManager[AnyIntParameter],
     results_manager: _ResultsManager,
     evaluation_directory: Path,
     model_type: str,
 ) -> None:
     variation_idxs_iter = product(
-        range(weather._n_variations),
-        *(range(parameter._n_variations) for parameter in parameters),
+        range(parameters_manager._weather._n_variations),
+        *(
+            range(parameter._n_variations)
+            for parameter in parameters_manager._parameters
+        ),
     )
+    jobs = tuple(parameters_manager._jobs(*variation_idxs_iter))  # type: ignore[arg-type] # might be resolved after python/mypy#12280
 
     _parallel_evaluate(
         _product_evaluate,
-        variation_idxs_iter,
+        jobs,
         results_manager,
         tagged_model=tagged_model,
-        weather=weather,
-        parameters=parameters,
+        parameters_manager=parameters_manager,
         evaluation_directory=evaluation_directory,
         model_type=model_type,
     )
