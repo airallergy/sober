79c0f5954aadb02c705bf36657c0bab1712ac72a
Cheng Cui <cheng.cui.95@gmail.com>
2022-07-04 21:15:23 +0100

    Add verbose typing for job tuples in, TODO: revision after 3.11

--- optimisation/config.py
+++ optimisation/config.py
@@ -10,8 +10,25 @@ from ._tools import AnyStrPath
 
 AnyModelType: TypeAlias = Literal[".idf", ".imf"]
 AnyLanguage: TypeAlias = Literal["python"]
-AnyVariationVec: TypeAlias = tuple[int, Unpack[tuple[str, ...]]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
-AnyUncertaintyVec: TypeAlias = tuple[int, Unpack[tuple[str, ...]]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
+
+IntVURow: TypeAlias = tuple[int, int]
+FloatVURow: TypeAlias = tuple[float]
+AnyVURow: TypeAlias = IntVURow | FloatVURow  # type: ignore[operator] #python/typeshed#4819
+AnyVariationVec: TypeAlias = tuple[int, Unpack[tuple[float, ...]]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
+AnyUncertaintyVec: TypeAlias = tuple[int, Unpack[tuple[float, ...]]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
+AnyVUMat: TypeAlias = tuple[
+    IntVURow, Unpack[tuple[AnyVURow, ...]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
+]
+AnyTask: TypeAlias = tuple[str, AnyVUMat]
+AnyJob: TypeAlias = tuple[str, tuple[AnyTask, ...]]
+## TODO: the following may be generalised after python/mypy#12280 ##
+AnyIntVariationVec: TypeAlias = tuple[int, ...]
+AnyIntUncertaintyVec: TypeAlias = tuple[int, ...]
+AnyIntVUMat: TypeAlias = tuple[IntVURow, ...]
+AnyIntTask: TypeAlias = tuple[str, AnyIntVUMat]
+AnyIntJob: TypeAlias = tuple[str, tuple[AnyIntTask, ...]]
+####################################################################
+
 AnyUIDs: TypeAlias = tuple[str, ...]
 AnyUIDsPair: TypeAlias = tuple[str, AnyUIDs]
 
