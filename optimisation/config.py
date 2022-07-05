93a9c8991cd33bed5312a35ae02eaac4c23ee6df
Cheng Cui <cheng.cui.95@gmail.com>
2022-07-05 02:17:51 +0100

    Clarify some type alias names

--- optimisation/config.py
+++ optimisation/config.py
@@ -11,20 +11,20 @@ from ._tools import AnyStrPath
 AnyModelType: TypeAlias = Literal[".idf", ".imf"]
 AnyLanguage: TypeAlias = Literal["python"]
 
-IntVURow: TypeAlias = tuple[int, int]
-FloatVURow: TypeAlias = tuple[float]
-AnyVURow: TypeAlias = IntVURow | FloatVURow  # type: ignore[operator] #python/typeshed#4819
+AnyIntVURow: TypeAlias = tuple[int, int]
+AnyFloatVURow: TypeAlias = tuple[float]
+AnyVURow: TypeAlias = AnyIntVURow | AnyFloatVURow  # type: ignore[operator] #python/typeshed#4819
 AnyVariationVec: TypeAlias = tuple[int, Unpack[tuple[float, ...]]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
 AnyUncertaintyVec: TypeAlias = tuple[int, Unpack[tuple[float, ...]]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
 AnyVUMat: TypeAlias = tuple[
-    IntVURow, Unpack[tuple[AnyVURow, ...]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
+    AnyIntVURow, Unpack[tuple[AnyVURow, ...]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
 ]
 AnyTask: TypeAlias = tuple[str, AnyVUMat]
 AnyJob: TypeAlias = tuple[str, tuple[AnyTask, ...]]
 ## TODO: the following may be generalised after python/mypy#12280 ##
 AnyIntVariationVec: TypeAlias = tuple[int, ...]
 AnyIntUncertaintyVec: TypeAlias = tuple[int, ...]
-AnyIntVUMat: TypeAlias = tuple[IntVURow, ...]
+AnyIntVUMat: TypeAlias = tuple[AnyIntVURow, ...]
 AnyIntTask: TypeAlias = tuple[str, AnyIntVUMat]
 AnyIntJob: TypeAlias = tuple[str, tuple[AnyIntTask, ...]]
 ####################################################################
