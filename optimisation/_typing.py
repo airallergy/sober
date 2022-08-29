5ba7af37e8be22171ff543a1cb2378993e118552
Cheng Cui <cheng.cui.95@gmail.com>
2022-08-30 00:48:33 +0100

    Fix AnyFloatVURow

--- optimisation/_typing.py
+++ optimisation/_typing.py
@@ -15,7 +15,7 @@ AnyModelType: TypeAlias = Literal[".idf", ".imf"]
 AnyLanguage: TypeAlias = Literal["python"]
 
 AnyIntVURow: TypeAlias = tuple[int, int]
-AnyFloatVURow: TypeAlias = tuple[float]
+AnyFloatVURow: TypeAlias = tuple[float, float]  # TODO: check this against tuple[float]
 AnyVURow: TypeAlias = AnyIntVURow | AnyFloatVURow  # type: ignore[operator] #python/typeshed#4819
 AnyVariationVec: TypeAlias = tuple[int, Unpack[tuple[float, ...]]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
 AnyUncertaintyVec: TypeAlias = tuple[int, Unpack[tuple[float, ...]]]  # type: ignore[misc] # python/mypy#12280 # TODO: Unpack -> * after 3.11
