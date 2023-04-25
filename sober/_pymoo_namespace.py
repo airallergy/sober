ad3181ccb80d412e9240a3f5dd3effc9e215552c
Cheng Cui <cheng.cui.95@gmail.com>
2023-04-25 02:22:49 +0100

    Delete unused variables in _pymoo_namespace.py

--- sober/_pymoo_namespace.py
+++ sober/_pymoo_namespace.py
@@ -8,7 +8,6 @@ from pymoo.core.termination import Termination
 from pymoo.operators.mutation.pm import PolynomialMutation
 from pymoo.operators.repair.rounding import RoundingRepair
 from pymoo.algorithms.moo.nsga2 import NSGA2, binary_tournament
-from pymoo.operators.sampling.lhs import LatinHypercubeSampling
 from pymoo.operators.crossover.sbx import SimulatedBinaryCrossover
 from pymoo.termination.max_gen import MaximumGenerationTermination
 from pymoo.algorithms.moo.nsga3 import NSGA3, comp_by_cv_then_random
