91abe615215e2d3ffa3e7f621a34c6d0f8627ff4
Cheng Cui <cheng.cui.95@gmail.com>
2023-04-27 02:35:00 +0100

    Fix pymoo0.6 performance regression due to change of operator arguments

--- sober/_optimiser.py
+++ sober/_optimiser.py
@@ -27,30 +27,31 @@ def _operators(
         "nsga3": {"crossover": 30, "mutation": 20},
     }
 
+    crossover_kwargs = dict(prob=p_crossover, eta=etas[algorithm_name]["crossover"])
+
+    # in pymoo0.6 mutation,
+    # prob (0.5) -> prob_var, controlling mutation for each gene/variable
+    # prob controls the whole mutation operation
+    # see https://github.com/anyoptimization/pymoo/discussions/360
+    # though the answer is not entirely correct
+    mutation_kwargs = dict(
+        prob=1.0, prob_var=p_mutation, eta=etas[algorithm_name]["mutation"]
+    )
+
     return {
         "sampling": pm.MixedVariableSampling(),
         "mating": pm.MixedVariableMating(
             selection=selections[algorithm_name],
             crossover={
-                pm.Real: pm.SimulatedBinaryCrossover(
-                    prob=p_crossover, eta=etas[algorithm_name]["crossover"]
-                ),
+                pm.Real: pm.SimulatedBinaryCrossover(**crossover_kwargs),
                 pm.Integer: pm.SimulatedBinaryCrossover(
-                    prob=p_crossover,
-                    eta=etas[algorithm_name]["crossover"],
-                    vtype=float,
-                    repair=pm.RoundingRepair(),
+                    **crossover_kwargs, vtype=float, repair=pm.RoundingRepair()
                 ),
             },
             mutation={
-                pm.Real: pm.PolynomialMutation(
-                    prob=p_mutation, eta=etas[algorithm_name]["mutation"]
-                ),
+                pm.Real: pm.PolynomialMutation(**mutation_kwargs),
                 pm.Integer: pm.PolynomialMutation(
-                    prob=p_mutation,
-                    eta=etas[algorithm_name]["mutation"],
-                    vtype=float,
-                    repair=pm.RoundingRepair(),
+                    **mutation_kwargs, vtype=float, repair=pm.RoundingRepair()
                 ),
             },
             eliminate_duplicates=pm.MixedVariableDuplicateElimination(),
