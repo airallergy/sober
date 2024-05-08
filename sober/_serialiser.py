37b047effd5d6ef5002ec2250434d0124511f925
Cheng Cui <cheng.cui.95@gmail.com>
2024-05-08 13:06:18 +0000

    Handle _STAR_ARG_NAMES smarter in _retraced_init_attr_names

--- sober/_serialiser.py
+++ sober/_serialiser.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 import abc
-import itertools as it
 import typing
 from typing import TYPE_CHECKING
 
@@ -53,14 +52,30 @@ def _retraced_init_attr_names(
     cls: type, key: Literal["_ARG_NAMES", "_STAR_ARG_NAMES", "_KWARG_NAMES"]
 ) -> tuple[str, ...]:
     """retraces init attribute names"""
-    return tuple(
-        it.chain.from_iterable(
-            names
-            for item in _sober_mro(cls)
-            if hasattr(item, key)
-            and (set(names := getattr(item, key)) <= set(item.__slots__))
-        )
-    )
+
+    attr_names = ()
+    for item in _sober_mro(cls):
+        # cls has to have the key
+        if not hasattr(item, key):
+            continue
+
+        names = getattr(item, key)
+
+        # names in key have to be a subset of cls's __slots__
+        if not (set(names) <= set(item.__slots__)):
+            continue
+
+        # only _STAR_ARG_NAMES in the leftmost mro is accounted
+        # NOTE: '_STAR_ARG_NAMES' is overriding
+        #       whilst other keys are concatenatig like __slots__
+        if (key == "_STAR_ARG_NAMES") and len(attr_names):
+            # NOTE: expected to only apply to _bounds vs _options
+            assert names == ("_bounds",)
+            continue
+
+        attr_names += names
+
+    return attr_names
 
 
 def _retraced_init_attr_names_map(cls: type) -> _InitAttrNames:
