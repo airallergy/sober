a2548e90fbdac8efb199cd2ff408461ce8b8e347
Cheng Cui <cheng.cui.95@gmail.com>
2024-11-24 23:29:37 +0000

    Bump version to 0.2.0

--- sober/__init__.py
+++ sober/__init__.py
@@ -14,7 +14,7 @@ from sober.input import (
 from sober.output import RVICollector, ScriptCollector
 from sober.problem import Problem
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 
 __all__ = (
     "CategoricalModifier",
